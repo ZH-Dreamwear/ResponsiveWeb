aa20ea2ff5ff775d1a258bba73a36beb8a428a9a
ZH-Dreamwear <1217243450@qq.com>
2018-05-17 13:06:35 +0800

    实现广告轮播图功能，具备响应式。

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*
+* @Author: Administrator
+* @Date:   2018-03-30 17:28:07
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-17 13:04:42
+*/
+$(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        loop: true
+    });
+});
